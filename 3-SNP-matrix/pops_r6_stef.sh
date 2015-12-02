2e8089f5564c57c6107ca30baee104f1fa070161
pesalerno <patriciasalerno@gmail.com>
2015-12-02 09:06:49 -0500

    fixed populations files

--- 3-SNP-matrix/pops_r6_stef.sh
+++ 3-SNP-matrix/pops_r6_stef.sh
@@ -8,4 +8,5 @@ cd $PBS_O_WORKDIR
 
 module load stacks/1.35
 
-populations -b 2 -P ./populations_r_6/ -M ./popmap_Stefania.txt -k -p 3 -r 0.6 -a 0.02 -f p_value -t 36 --structure --genepop --vcf --write_random_snp
+
+populations -b 2 -P . -M ./popmap-Stefania-c.txt -k -p 3 -r 0.6 -a 0.02 -f p_value -t 36 --structure --genepop --vcf --write_random_snp
